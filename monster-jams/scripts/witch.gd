1a7900926b7514eaf7c3455a21a0c25179520756
cgpinta <pinta.christian@outlook.com>
2025-10-31 00:33:59 -0500

    improved witch curse animation a ton

--- monster-jams/scripts/witch.gd
+++ monster-jams/scripts/witch.gd
@@ -36,9 +36,21 @@ func _ready():
 
 var strCAST: String = "cast"
 
-var PRE_CAST_LENGTH: float = 0.5
-var PRE_CAST_DEPTH: float = 0.001
-var POST_CAST_LENGTH: float = 0.5
+var SHAKE_RATE: float = 0.05
+var SHAKE_DIST: float = 0.01
+var shakeCount: int = 0
+var shakeTimer: float = 0
+
+var PRE_CAST_FALL_RATE: float = 0.1
+
+var PRE_CAST_LENGTH: float = 1
+var POST_CAST_DEPTH: float = 0.025
+var POST_CAST_FALL_RATE: float = 0.05
+var postCastFallSpeed: float = 0.01
+var POST_CAST_LENGTH: float = 2
+
+var WITCH_TEXT: String = "Witch cursed you!"
+var WITCH_TEXT_TIME: float = 2
 
 var castAnimPhase: int = 0 
 
@@ -47,13 +59,14 @@ var ogSpritePos: Vector3
 func at_counter_angry_reaction():
 	ogSpritePos = sprite.position
 	castAnimPhase = 1
-	sprite.position.y = -PRE_CAST_DEPTH
 	await get_tree().create_timer(PRE_CAST_LENGTH, true, false,true).timeout
 	castAnimPhase = 2
 	sprite.play(strCAST)
-	sprite.position = ogSpritePos
+	GM.ui.set_center_text(WITCH_TEXT, WITCH_TEXT_TIME)
+	#sprite.position = ogSpritePos
 	play_rand_special()
 	sprite.position = ogSpritePos
+	sprite.position.y += POST_CAST_DEPTH
 	await get_tree().create_timer(POST_CAST_LENGTH, true, false,true).timeout
 	sprite.play("default")
 	orderTimedOut.emit(self, false)
@@ -64,12 +77,26 @@ func _process(delta):
 	if posState == MonsterPositionState.Angry:
 		if castAnimPhase != 0:
 			if castAnimPhase == 1:
-				if sprite.position.y < ogSpritePos.y:
-					sprite.position.y += delta
+				sprite.position.y -= delta * PRE_CAST_FALL_RATE
+				if shakeTimer > 0:
+					shakeTimer -= delta
+					pass
+				else:
+					shakeTimer = SHAKE_RATE
+					shakeCount += 1
+					if shakeCount % 2 == 0:
+						sprite.position.x = SHAKE_DIST
+					else:
+						sprite.position.x = -SHAKE_DIST
+					pass
+				pass
+			elif castAnimPhase == 2:
+				if sprite.position.y > ogSpritePos.y:
+					postCastFallSpeed -= POST_CAST_FALL_RATE * delta
+					sprite.position.y += postCastFallSpeed
 					pass
 				else:
 					sprite.position.y = ogSpritePos.y
-					pass
 				pass
 			pass
 	pass
